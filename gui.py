64b1a6cc30c49b9dc01b05866ca3565270c96ebb
Oliver <mail@oliver-kent.co.uk>
2018-02-12 16:28:55 +0000

    gui.py * Moved connecting callbacks into its own func * Created a timer to repeatedly check for changes to body text and save them to the storage (memory) * Added callback to refresh index instantly when note title changes and save to storage * Init now opens the first note
    TODO: Better class tidy up, and better current note/selected item handling

--- gui.py
+++ gui.py
@@ -29,47 +29,94 @@ class GUI(QtWidgets.QApplication):
         self.dictata = dictata
         self.current_note = None
 
-        self.editor.notes_treeview.clicked.connect(self.click_treeview)
-        self.editor.sync_button.clicked.connect(self.click_syncbutton)
-        self.editor.delete_button.clicked.connect(self.click_deletebutton)
+        self.sync_timer = QtCore.QTimer()
+        self.sync_timer.start(5000)
+        self.sync_timer.timeout.connect(self._sync_timercallback)
+
+        self.tree_model = self.editor.notes_treeview.model()
 
         self.refresh_indextree()
+
+        self.editor.notes_treeview.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+
+        self.connect_callbacks()
+
+        self.open_note(note_index=self.tree_model.index(0, 0))
+
+    def connect_callbacks(self):
+
+        self.editor.delete_button.clicked.connect(self._click_deletebutton)
+        self.sync_timer.timeout.connect(self._sync_timercallback)
+        self.editor.notes_treeview.customContextMenuRequested.connect(self.contextMenuEvent)
+        self.editor.notes_treeview.clicked.connect(self._click_notestreeview)
+        self.editor.title_lineedit.textChanged.connect(self.refresh_indextree)
+
+    def _sync_timercallback(self):
+        if self.editor.body_textedit.document().isModified():
+            self.editor.body_textedit.document().setModified(False)
+            self.current_note.body = self.editor.body_textedit.toPlainText()
+            self.dictata.storage.sync()
 
     def refresh_indextree(self):
-        tree_model = self.editor.notes_treeview.model()
-        tree_model.removeRows(0, tree_model.rowCount())
+        if self.current_note:
+            self.current_note.title = self.editor.title_lineedit.text()
+        self.tree_model.removeRows(0, self.tree_model.rowCount())
         for row, note in enumerate(self.dictata.note_list.notes.values()):
-            tree_model.insertRow(row)
+            self.tree_model.insertRow(row)
             self.setTreeData(row, 0, note.title, note.uid)
-        row = tree_model.rowCount()
-        tree_model.insertRow(row)
-        self.setTreeData(row, 0, "Add new note", "NEW NOTE")
+        row = self.tree_model.rowCount()
+        self.tree_model.insertRow(row)
+        self.setTreeData(row, 0, "+", "")
 
-    def click_syncbutton(self):
-        self.current_note.title = self.editor.title_lineedit.text()
-        self.current_note.body = self.editor.body_textedit.toPlainText()
-        self.dictata.storage.sync()
-        self.refresh_indextree()
-
-    def click_deletebutton(self):
+    def _click_deletebutton(self):
+        #self.open_note(self.editor.notes_treeview.indexAbove()
         self.dictata.note_list.delete(self.current_note.uid)
         self.refresh_indextree()
 
-    def click_treeview(self, index):
+    def _click_notestreeview(self, index):
         uid = self.editor.notes_treeview.model().data(index, QtCore.Qt.UserRole)
-        self.open_note(uid)
+        if uid:
+            self.sync_current_note()
+            self.open_note(uid)
+
+    def sync_current_note(self):
+        if self.current_note:
+            self.current_note.title = self.editor.title_lineedit.text()
+            self.current_note.body = self.editor.body_textedit.toPlainText()
+            self.dictata.storage.sync()
 
     def setTreeData(self, row, col, display_data, user_data):
         tree_model = self.editor.notes_treeview.model()
         tree_model.setData(tree_model.index(row, col), display_data, QtCore.Qt.DisplayRole)
         tree_model.setData(tree_model.index(row, col), user_data, QtCore.Qt.UserRole)
 
-    def open_note(self, note_uid):
-        self.current_note = self.dictata.note_list.notes[note_uid]
+    def open_note(self, note_uid=None, note_index=None):
+        if note_uid:
+            self.current_note = self.dictata.note_list.notes[note_uid]
+        elif note_index:
+            uid = self.editor.notes_treeview.model().data(note_index, QtCore.Qt.UserRole)
+            self.current_note = self.dictata.note_list.notes[uid]
+
         self.editor.title_lineedit.setText(self.current_note.title)
         self.editor.body_textedit.clear()
         self.editor.body_textedit.append(self.current_note.body)
 
+    def contextMenuEvent(self, cursor_position):
+        row = self.editor.notes_treeview.indexAt(cursor_position)
+        self.menu = QtWidgets.QMenu()
+        action = self.menu.addAction("Delete note")
+        action.setData("DELETE")
+        #action
+        self.menu.addAction("Insert new note", )
+        self.menu.addAction("Duplicate note")
+        self.menu.triggered.connect(self.pop)
+        self.menu.exec(self.editor.notes_treeview.mapToGlobal(cursor_position))
+        pass
+
+    def pop(self, action):
+        t = action.text()
+        d = action.data()
+        pass
 
     def show(self):
         self.editor.show()
